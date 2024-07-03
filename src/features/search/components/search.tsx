eec40da7f775ba8b6ef007adf2ffb987c20c22bd
Joe Rosseljong <general@veepzy.net>
2024-07-03 19:46:31 +0200

    feat: display items

--- src/features/search/components/search.tsx
+++ src/features/search/components/search.tsx
@@ -10,8 +10,16 @@ import {
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import { searchQuery } from "../api/search";
+import { useState } from "react";
+import { youtube_v3 } from "googleapis";
+import Items from "./items";
+import { Separator } from "@/components/ui/separator";
 
 const Search: React.FC = () => {
+    const [items, setItems] = useState<youtube_v3.Schema$SearchResult[]>(
+        [],
+    );
+
     const form = useForm({
         defaultValues: {
             search: "",
@@ -20,31 +28,47 @@ const Search: React.FC = () => {
 
     const onSubmit = async (data: { search: string }) => {
         const response = await searchQuery(data.search);
+        if (!response) return;
         console.log(response);
+        setItems(response);
     };
 
     return (
-        <div className="space-between flex items-center">
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <FormField
-                        control={form.control}
-                        name="search"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormControl>
-                                    <Input
-                                        {...field}
-                                        placeholder="Search"
-                                        className="w-max-[300px]"
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </form>
-            </Form>
+        <div className="h-full space-y-6">
+            <div className="space-between flex items-center">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                            control={form.control}
+                            name="search"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder="Search"
+                                            className="w-max-[300px]"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+            </div>
+            <div className="flex flex-col">
+                <div className="space-y-1">
+                    <h2 className="text-2xl font-semibold tracking-tight">
+                        Search results
+                    </h2>
+                    <p className="text-muted-foreground">
+                        {items.length} results found
+                    </p>
+                </div>
+                <Separator className="my-4" />
+                <Items items={items} />
+            </div>
         </div>
     );
 };
