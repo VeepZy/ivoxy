49d752df8a8eeb5a3975f092f94b0c6495e99d0d
Joe Rosseljong <general@veepzy.net>
2024-07-03 15:32:52 +0200

    feat: menu bar with kinde auth

---
+++ src/features/menu/components/menu.tsx
@@ -0,0 +1,81 @@
+"use server";
+
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger,
+} from "@/components/ui/menubar";
+import {
+    LoginLink,
+    RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Menu: React.FC = async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    return (
+        <Menubar className="rounded-none border-none px-2 lg:px-4">
+            <MenubarMenu>
+                <MenubarTrigger className="font-bold">
+                    Ivoxy
+                </MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem>About Ivoxy</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Preferences</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Quit</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger className="relative">File</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarSub>
+                        <MenubarSubTrigger>New</MenubarSubTrigger>
+                        <MenubarSubContent className="w-[230px]">
+                            <MenubarItem>Playlist</MenubarItem>
+                        </MenubarSubContent>
+                    </MenubarSub>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>View</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem>Show Playing Next</MenubarItem>
+                    <MenubarItem>Hide Sidebar</MenubarItem>
+                    <MenubarItem>Enter Full Screen</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Account</MenubarTrigger>
+                {user ? (
+                    <MenubarContent>
+                        <MenubarItem>Profile</MenubarItem>
+                        <MenubarItem>Settings</MenubarItem>
+                        <MenubarItem>Logout</MenubarItem>
+                    </MenubarContent>
+                ) : (
+                    <MenubarContent>
+                        <MenubarItem>
+                            <LoginLink>Sign In</LoginLink>
+                        </MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarItem>
+                            <RegisterLink>Register</RegisterLink>
+                        </MenubarItem>
+                    </MenubarContent>
+                )}
+            </MenubarMenu>
+        </Menubar>
+    );
+};
+
+export default Menu;
