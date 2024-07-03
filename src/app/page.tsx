96709882b232e1bfd5196bede0cb2af091ab6a96
Joe Rosseljong <general@veepzy.net>
2024-07-03 15:32:04 +0200

    feat: add menu and sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+import Menu from "@/features/menu/components/menu";
+import Sidebar from "@/features/sidebar/components/sidebar";
 import { type NextPage } from "next";
 
 const HomeRoute: NextPage = () => {
     return (
-        <div>
-            <p>Home</p>
-        </div>
+        <>
+            <div className="hidden md:block">
+                <Menu />
+                <div className="border-t">
+                    <div className="bg-background">
+                        <div className="grid lg:grid-cols-5">
+                            <Sidebar />
+                            <div className="col-span-3 lg:col-span-4 lg:border-l">
+                                <div className="h-full px-4 py-6 lg:px-8"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
