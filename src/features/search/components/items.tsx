09310cd7e3f9dfae079cac54dd02fdb60903d5dc
Joe Rosseljong <general@veepzy.net>
2024-07-03 23:30:30 +0200

    feat: change video player url on click (broken)

--- src/features/search/components/items.tsx
+++ src/features/search/components/items.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePlayerState } from "@/hooks/state";
 import { unescapeHTML } from "@/lib/utils";
 import { youtube_v3 } from "googleapis";
 import Image from "next/image";
@@ -5,11 +8,20 @@ import Image from "next/image";
 const Items: React.FC<{ items: youtube_v3.Schema$SearchResult[] }> = ({
     items,
 }) => {
+    const { setUrl } = usePlayerState();
+
     return (
         <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
             {items.map((item) => (
                 <div key={item.etag} className="w-[320px] space-y-3">
-                    <div className="overflow-hidden rounded-md">
+                    <button
+                        className="overflow-hidden rounded-md ring-2 ring-destructive"
+                        onClick={() =>
+                            setUrl(
+                                `https://www.youtube.com/embed/${item.id?.videoId ?? null}`,
+                            )
+                        }
+                    >
                         <Image
                             src={
                                 item.snippet?.thumbnails?.medium?.url ?? ""
@@ -19,12 +31,12 @@ const Items: React.FC<{ items: youtube_v3.Schema$SearchResult[] }> = ({
                             alt={item.snippet?.title ?? ""}
                             className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
                         />
-                    </div>
+                    </button>
                     <div className="space-y-1 text-sm">
                         <h3 className="font-semibold leading-none">
                             {unescapeHTML(item.snippet?.title ?? "")}
                         </h3>
-                        <p className="text-muted-foreground text-xs">
+                        <p className="text-xs text-muted-foreground">
                             {item.snippet?.channelTitle ?? ""}
                         </p>
                     </div>
