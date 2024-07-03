f2221b6bcbaa0de61b7c5ce2047806c2287b3389
Joe Rosseljong <general@veepzy.net>
2024-07-03 19:02:35 +0200

    feat: handle google oauth callback

---
+++ src/app/api/auth/google/route.ts
@@ -0,0 +1,48 @@
+import { getAuthenticatedClient } from "@/lib/auth";
+import { oAuth2Client } from "@/lib/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import url from "url";
+
+export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+    let query = url.parse(req.url ?? "", true)?.query;
+
+    if (!query)
+        return NextResponse.json(
+            { error: "No query provided" },
+            { status: 400 },
+        );
+
+    if (query.error) {
+        return NextResponse.json({ error: query.error }, { status: 400 });
+    } else {
+        const client = await getAuthenticatedClient(oAuth2Client);
+        const { tokens } = await client.getToken({
+            code: query.code?.toString() ?? "",
+        });
+
+        if (!tokens)
+            return NextResponse.json(
+                { error: "No tokens" },
+                { status: 400 },
+            );
+
+        client.setCredentials(tokens);
+        const cookieStore = cookies();
+        cookieStore.set(
+            "google_access_token",
+            tokens.access_token?.toString() ?? "",
+        );
+        cookieStore.set(
+            "google_refresh_token",
+            tokens.refresh_token?.toString() ?? "",
+        );
+        cookieStore.set(
+            "google_expiry_date",
+            tokens.expiry_date?.toString() ?? "",
+        );
+
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+};
