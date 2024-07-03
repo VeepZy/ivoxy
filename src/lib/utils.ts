35f7c2576b127fe50a41c0c72e05a006f21a1ef0
Joe Rosseljong <general@veepzy.net>
2024-07-03 19:46:49 +0200

    feat: unescape html entities in youtube title

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export const unescapeHTML = (html: string) => {
+    const parser = new DOMParser();
+    return (
+        parser.parseFromString(html, "text/html").body.textContent ?? ""
+    );
+};
