df1fc15aa1eec677eea04a64d3644578f9d1c0aa
Joe Rosseljong <general@veepzy.net>
2024-07-03 19:06:11 +0200

    refactor: remove existing client check

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,42 +2,32 @@
 
 import { OAuth2Client } from "google-auth-library";
 import { redirect } from "next/navigation";
-import { assignClient, oAuth2Client } from "./client";
 import { cookies } from "next/headers";
 
-export const getAuthenticatedClient = async (
-    client: OAuth2Client | null,
-) => {
-    if (!client) {
-        console.log("Creating new client");
-        client = await new OAuth2Client(
-            process.env.YOUTUBE_CLIENT_ID ?? "",
-            process.env.YOUTUBE_CLIENT_SECRET ?? "",
-            process.env.YOUTUBE_REDIRECT_URI ?? "",
-        );
+export const getAuthenticatedClient = async () => {
+    const client = await new OAuth2Client(
+        process.env.YOUTUBE_CLIENT_ID ?? "",
+        process.env.YOUTUBE_CLIENT_SECRET ?? "",
+        process.env.YOUTUBE_REDIRECT_URI ?? "",
+    );
 
-        const acccess_token = cookies().get("google_access_token");
-        const refresh_token = cookies().get("google_refresh_token");
-        const expiry_date = cookies().get("google_expiry_date");
+    const acccess_token = cookies().get("google_access_token");
+    const refresh_token = cookies().get("google_refresh_token");
+    const expiry_date = cookies().get("google_expiry_date");
 
-        if (acccess_token && refresh_token && expiry_date) {
-            client.setCredentials({
-                access_token: acccess_token.value,
-                refresh_token: refresh_token.value,
-                expiry_date: Number(expiry_date.value),
-            });
-        }
-
-        assignClient(client);
-        return client;
+    if (acccess_token && refresh_token && expiry_date) {
+        client.setCredentials({
+            access_token: acccess_token.value,
+            refresh_token: refresh_token.value,
+            expiry_date: Number(expiry_date.value),
+        });
     }
 
-    console.log("Returning existing client");
     return client;
 };
 
 export const auth = async () => {
-    const client = await getAuthenticatedClient(oAuth2Client);
+    const client = await getAuthenticatedClient();
 
     const scopes = [
         "https://www.googleapis.com/auth/youtube",
