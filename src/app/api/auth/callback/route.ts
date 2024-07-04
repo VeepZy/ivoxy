fb5b598d1c2ff9ee1ffd579e2d15a57386547e8b
Joe Rosseljong <general@veepzy.net>
2024-07-04 21:11:45 +0200

    feat: add callback url for supabase auth

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,22 @@
+import { createServerDBClient } from "@/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const { searchParams, origin } = new URL(request.url);
+    const code = searchParams.get("code");
+    const next = searchParams.get("next") ?? "/";
+
+    if (code) {
+        const db = createServerDBClient();
+
+        const { error } = await db.auth.exchangeCodeForSession(code);
+
+        if (!error) {
+            return NextResponse.redirect(`${origin}${next}`);
+        }
+    }
+
+    return NextResponse.redirect(
+        `${origin}?error=${encodeURIComponent("Failed to exchange code for session")}`,
+    );
+}
