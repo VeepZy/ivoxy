de1bac43271fa7f7323a5ba3765eed09dc6a559d
Joe Rosseljong <general@veepzy.net>
2024-07-04 18:33:22 +0200

    feat: format duration text and move domparser function to client

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,9 +5,10 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export const unescapeHTML = (html: string) => {
-    const parser = new DOMParser();
-    return (
-        parser.parseFromString(html, "text/html").body.textContent ?? ""
-    );
+export const formatDuration = (value: number) => {
+    const minutes = Math.floor(value / 60);
+    const seconds = Math.floor(value % 60);
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
+    return `${formattedMinutes}:${formattedSeconds}`;
 };
