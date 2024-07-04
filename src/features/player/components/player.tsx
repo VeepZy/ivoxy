c1e6726edd4a297aaeae46cb2cb2baf3b3971fe8
Joe Rosseljong <general@veepzy.net>
2024-07-04 14:22:19 +0200

    refactor: use context instead of hook

--- src/features/player/components/player.tsx
+++ src/features/player/components/player.tsx
@@ -4,13 +4,42 @@ import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { usePlayerState } from "@/hooks/state";
 import { PauseIcon, PlayIcon } from "lucide-react";
-import { Suspense, useRef } from "react";
+import { Suspense, useContext, useRef } from "react";
 import ReactPlayer from "react-player";
+import { PlayerContext } from "./context";
 
 const VideoPlayer: React.FC = () => {
     const player = useRef<ReactPlayer>(null);
-    const { state, onPlay, onPause, onEnded, onProgress, onDuration } =
-        usePlayerState();
+    const { state, setState, setUrl } = useContext(PlayerContext);
+
+    const onPlay = () =>
+        setState((prevState) => ({
+            ...prevState,
+            playing: true,
+        }));
+    const onPause = () =>
+        setState((prevState) => ({
+            ...prevState,
+            playing: false,
+        }));
+    const onEnded = () =>
+        setState((prevState) => ({
+            ...prevState,
+            playing: false,
+        }));
+    const onProgress = (event: {
+        playedSeconds: number;
+        loadedSeconds: number;
+    }) =>
+        setState((prevState) => ({
+            ...prevState,
+            played: event.playedSeconds,
+        }));
+    const onDuration = (duration: number) =>
+        setState((prevState) => ({
+            ...prevState,
+            duration,
+        }));
 
     return (
         <div className="fixed bottom-0 left-0 right-0 flex h-16 w-full items-center border-t bg-card shadow-lg">
@@ -22,13 +51,20 @@ const VideoPlayer: React.FC = () => {
                 >
                     {state.playing ? <PauseIcon /> : <PlayIcon />}
                 </Button>
+                <Button
+                    onClick={() =>
+                        setUrl("https://www.youtube.com/embed/S2jwrC2a1p8")
+                    }
+                >
+                    setUrl
+                </Button>
                 <pre>{JSON.stringify(state)}</pre>
             </div>
 
             <div className="absolute bottom-16 left-0 right-0">
                 <Progress
                     value={state.played}
-                    max={285}
+                    max={state.duration}
                     className="h-2 rounded-none"
                 />
             </div>
