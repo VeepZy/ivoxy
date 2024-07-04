f37acbf53d70bf5f7d031636d37e8052bc07b8cf
Joe Rosseljong <general@veepzy.net>
2024-07-04 14:22:57 +0200

    refactor: remove player controls and move them to the player

--- src/hooks/state.ts
+++ src/hooks/state.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const usePlayerState = () => {
     const [state, setState] = useState(() => ({
@@ -14,58 +14,5 @@ export const usePlayerState = () => {
         loaded: 0,
     }));
 
-    const setUrl = (newUrl: string) => {
-        console.log("SET URL", state.url, newUrl);
-        setState((prevState) => ({
-            ...prevState,
-            url: newUrl,
-            played: 0,
-            loaded: 0,
-            playing: false,
-        }));
-    };
-
-    const onPlay = () => {
-        console.log("ON PLAY");
-        setState((prevState) => ({ ...prevState, playing: true }));
-    };
-
-    const onPause = () => {
-        console.log("ON PAUSE");
-        setState((prevState) => ({ ...prevState, playing: false }));
-    };
-
-    const onEnded = () => {
-        console.log("ON ENDED");
-        setState((prevState) => ({ ...prevState, playing: false }));
-    };
-
-    const onProgress = (event: {
-        playedSeconds: number;
-        loadedSeconds: number;
-    }) => {
-        console.log("ON PROGRESS", event);
-        setState((prevState) => ({
-            ...prevState,
-            played: event.playedSeconds,
-        }));
-    };
-
-    const onDuration = (duration: number) => {
-        console.log("ON DURATION", duration);
-        setState((prevState) => ({
-            ...prevState,
-            duration: duration,
-        }));
-    };
-
-    return {
-        state,
-        onPlay,
-        onPause,
-        onEnded,
-        onProgress,
-        onDuration,
-        setUrl,
-    };
+    return [state, setState] as const;
 };
