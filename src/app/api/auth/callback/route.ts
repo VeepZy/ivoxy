4b45bfb65ba215e5afd6f34f4545ca69e87aaf00
Joe Rosseljong <general@veepzy.net>
2024-07-05 14:24:24 +0200

    fix: imports

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,5 +1,6 @@
+import { type NextRequest, NextResponse } from "next/server";
+
 import { createServerDBClient } from "@/db";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const { searchParams, origin } = new URL(request.url);
