d208500cfc9c893850f7ac2c8da760f8d622600d
Joe Rosseljong <general@veepzy.net>
2024-07-05 14:27:12 +0200

    fix: imports and prefer named export

--- src/features/menu/components/sign-out.tsx
+++ src/features/menu/components/sign-out.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { MenubarItem } from "@/components/ui/menubar";
-import { logout } from "../api/logout";
+
+import { useLogout } from "../api/logout";
 
 const SignOut: React.FC = () => {
-    return <MenubarItem onClick={() => logout()}>Sign Out</MenubarItem>;
+    const logout = useLogout();
+
+    return <MenubarItem onClick={() => logout}>Sign Out</MenubarItem>;
 };
 
-export default SignOut;
+export { SignOut };
