a8a07e8c73837ecf0405f86ce4c08b8aaea36249
Joe Rosseljong <general@veepzy.net>
2024-07-05 16:22:34 +0200

    refactor: replace type and use data object instead of string

--- src/app/playlists/play.tsx
+++ src/app/playlists/play.tsx
@@ -4,10 +4,8 @@ import { PlayIcon } from "lucide-react";
 import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
-import { type Database } from "@/db/schema";
 import { PlayerContext } from "@/features/player/components/context";
-
-type Playlist = Database["public"]["Tables"]["playlists"]["Row"];
+import { Playlist } from "@/db/types";
 
 const PlayPlaylist: React.FC<{ playlist: Playlist }> = ({ playlist }) => {
     const { setUrl } = useContext(PlayerContext);
@@ -17,7 +15,7 @@ const PlayPlaylist: React.FC<{ playlist: Playlist }> = ({ playlist }) => {
             className="absolute inset-0 bottom-0 left-0 right-0 top-0 z-10 h-full hover:bg-transparent"
             variant="ghost"
             onClick={() => {
-                setUrl(playlist.urls);
+                setUrl(playlist.data);
             }}
         >
             <span className="sr-only">Play</span>
