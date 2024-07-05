5bac0601a9e8136de1052723e2e34e75e1753329
Joe Rosseljong <general@veepzy.net>
2024-07-05 14:28:59 +0200

    fix: import and prop order

--- src/features/sidebar/components/sidebar.tsx
+++ src/features/sidebar/components/sidebar.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Database } from "@/db/schema";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { type Database } from "@/db/schema";
+import { cn } from "@/lib/utils";
 
 type Playlist = Database["public"]["Tables"]["playlists"]["Row"];
 
@@ -21,18 +22,18 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                     </h2>
                     <div className="space-y-1">
                         <Button
-                            variant="secondary"
                             className="w-full justify-start"
+                            variant="secondary"
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
+                                className="mr-2 h-4 w-4"
                                 fill="none"
                                 stroke="currentColor"
-                                strokeWidth="2"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
                             >
                                 <circle cx="12" cy="12" r="10" />
                                 <polygon points="10 8 16 12 10 16 10 8" />
@@ -40,66 +41,69 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                             Listen Now
                         </Button>
                         <Button
+                            asChild
                             variant="ghost"
                             className={cn("w-full justify-start", {
                                 "text-destructive hover:text-destructive":
                                     pathname === "/browse",
                             })}
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
-                            >
-                                <rect
-                                    width="7"
-                                    height="7"
-                                    x="3"
-                                    y="3"
-                                    rx="1"
-                                />
-                                <rect
-                                    width="7"
-                                    height="7"
-                                    x="14"
-                                    y="3"
-                                    rx="1"
-                                />
-                                <rect
-                                    width="7"
-                                    height="7"
-                                    x="14"
-                                    y="14"
-                                    rx="1"
-                                />
-                                <rect
-                                    width="7"
-                                    height="7"
-                                    x="3"
-                                    y="14"
-                                    rx="1"
-                                />
-                            </svg>
-                            <Link href="/browse">Browse</Link>
+                            <Link href="/browse">
+                                <svg
+                                    className="mr-2 h-4 w-4"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <rect
+                                        height="7"
+                                        rx="1"
+                                        width="7"
+                                        x="3"
+                                        y="3"
+                                    />
+                                    <rect
+                                        height="7"
+                                        rx="1"
+                                        width="7"
+                                        x="14"
+                                        y="3"
+                                    />
+                                    <rect
+                                        height="7"
+                                        rx="1"
+                                        width="7"
+                                        x="14"
+                                        y="14"
+                                    />
+                                    <rect
+                                        height="7"
+                                        rx="1"
+                                        width="7"
+                                        x="3"
+                                        y="14"
+                                    />
+                                </svg>
+                                Browse
+                            </Link>
                         </Button>
                         <Button
-                            variant="ghost"
                             className="w-full justify-start"
+                            variant="ghost"
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
+                                className="mr-2 h-4 w-4"
                                 fill="none"
                                 stroke="currentColor"
-                                strokeWidth="2"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
                             >
                                 <path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9" />
                                 <path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5" />
@@ -117,40 +121,43 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                     </h2>
                     <div className="space-y-1">
                         <Button
-                            variant="ghost"
+                            asChild
                             className="w-full justify-start"
+                            variant="ghost"
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
-                            >
-                                <path d="M21 15V6" />
-                                <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                                <path d="M12 12H3" />
-                                <path d="M16 6H3" />
-                                <path d="M12 18H3" />
-                            </svg>
-                            Playlists
+                            <Link href="/playlists">
+                                <svg
+                                    className="mr-2 h-4 w-4"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M21 15V6" />
+                                    <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                                    <path d="M12 12H3" />
+                                    <path d="M16 6H3" />
+                                    <path d="M12 18H3" />
+                                </svg>
+                                Playlists
+                            </Link>
                         </Button>
                         <Button
-                            variant="ghost"
                             className="w-full justify-start"
+                            variant="ghost"
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
+                                className="mr-2 h-4 w-4"
                                 fill="none"
                                 stroke="currentColor"
-                                strokeWidth="2"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
                             >
                                 <circle cx="8" cy="18" r="4" />
                                 <path d="M12 18V2l7 4" />
@@ -158,18 +165,18 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                             Songs
                         </Button>
                         <Button
-                            variant="ghost"
                             className="w-full justify-start"
+                            variant="ghost"
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
+                                className="mr-2 h-4 w-4"
                                 fill="none"
                                 stroke="currentColor"
-                                strokeWidth="2"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
                             >
                                 <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
                                 <circle cx="12" cy="7" r="4" />
@@ -177,18 +184,18 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                             Made for You
                         </Button>
                         <Button
-                            variant="ghost"
                             className="w-full justify-start"
+                            variant="ghost"
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
+                                className="mr-2 h-4 w-4"
                                 fill="none"
                                 stroke="currentColor"
-                                strokeWidth="2"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
                             >
                                 <path d="m12 8-9.04 9.06a2.82 2.82 0 1 0 3.98 3.98L16 12" />
                                 <circle cx="17" cy="7" r="5" />
@@ -196,18 +203,18 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                             Artists
                         </Button>
                         <Button
-                            variant="ghost"
                             className="w-full justify-start"
+                            variant="ghost"
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
+                                className="mr-2 h-4 w-4"
                                 fill="none"
                                 stroke="currentColor"
-                                strokeWidth="2"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
                             >
                                 <path d="m16 6 4 14" />
                                 <path d="M12 6v14" />
@@ -226,18 +233,19 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                         <div className="space-y-1 p-2">
                             {playlists.map((playlist) => (
                                 <Button
-                                    variant="ghost"
+                                    key={playlist.id}
                                     className="w-full justify-start font-normal"
+                                    variant="ghost"
                                 >
                                     <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
+                                        className="mr-2 h-4 w-4"
                                         fill="none"
                                         stroke="currentColor"
-                                        strokeWidth="2"
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
-                                        className="mr-2 h-4 w-4"
+                                        strokeWidth="2"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path d="M21 15V6" />
                                         <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
@@ -256,4 +264,4 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
     );
 };
 
-export default Sidebar;
+export { Sidebar };
