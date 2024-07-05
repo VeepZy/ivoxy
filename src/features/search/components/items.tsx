85226482db71fb5f07bb13dc83f29b29a2e94d11
Joe Rosseljong <general@veepzy.net>
2024-07-05 14:28:38 +0200

    fix: import and "string | null" template

--- src/features/search/components/items.tsx
+++ src/features/search/components/items.tsx
@@ -1,25 +1,26 @@
 "use client";
 
-import { PlayerContext } from "@/features/player/components/context";
-import { usePlayerState } from "@/hooks/state";
-import { unescapeHTML } from "@/lib/utils.client";
-import { youtube_v3 } from "googleapis";
+import { type youtube_v3 as Youtube } from "googleapis";
+import { PlayIcon } from "lucide-react";
 import Image from "next/image";
 import { useContext } from "react";
 
-const Items: React.FC<{ items: youtube_v3.Schema$SearchResult[] }> = ({
+import { PlayerContext } from "@/features/player/components/context";
+import { unescapeHTML } from "@/lib/utils.client";
+
+const Items: React.FC<{ items: Youtube.Schema$SearchResult[] }> = ({
     items,
 }) => {
     const { setUrl, setTitle } = useContext(PlayerContext);
 
-    const onSubmit = (item: youtube_v3.Schema$SearchResult) => {
+    const onSubmit = (item: Youtube.Schema$SearchResult) => {
+        if (!item.id?.videoId) return;
+
         setTitle(
             item.snippet?.title ?? "",
             item.snippet?.channelTitle ?? "",
         );
-        setUrl(
-            `https://www.youtube.com/embed/${item.id?.videoId ?? null}`,
-        );
+        setUrl([`https://www.youtube.com/embed/${item.id.videoId}`]);
     };
 
     return (
@@ -27,18 +28,22 @@ const Items: React.FC<{ items: youtube_v3.Schema$SearchResult[] }> = ({
             {items.map((item) => (
                 <div key={item.etag} className="w-[320px] space-y-3">
                     <button
-                        className="overflow-hidden rounded-md ring-2 ring-destructive"
+                        type="button"
+                        className="relative overflow-hidden rounded-md ring-2 ring-destructive"
                         onClick={() => onSubmit(item)}
                     >
                         <Image
+                            alt={item.snippet?.title ?? ""}
+                            className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
+                            height={180}
+                            width={320}
                             src={
                                 item.snippet?.thumbnails?.medium?.url ?? ""
                             }
-                            width={320}
-                            height={180}
-                            alt={item.snippet?.title ?? ""}
-                            className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
                         />
+                        <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:opacity-100">
+                            <PlayIcon className="h-10 w-10 text-white" />
+                        </div>
                     </button>
                     <div className="space-y-1 text-sm">
                         <h3 className="font-semibold leading-none">
@@ -54,4 +59,4 @@ const Items: React.FC<{ items: youtube_v3.Schema$SearchResult[] }> = ({
     );
 };
 
-export default Items;
+export { Items };
