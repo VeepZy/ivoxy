b23db281a5e337dcf578ab54c66f5941caa61f44
Joe Rosseljong <general@veepzy.net>
2024-07-05 16:23:28 +0200

    feat: add playlist type for jsonb data

---
+++ src/db/types.ts
@@ -0,0 +1,10 @@
+import { Database } from "./schema";
+
+type PlaylistRow = Database["public"]["Tables"]["playlists"]["Row"];
+export interface Playlist extends Omit<PlaylistRow, "data"> {
+    data: {
+        title: string;
+        url: string;
+        channelTitle: string;
+    }[];
+}
