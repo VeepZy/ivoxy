871e417e16367ae72af20dd79d7104bc68ffa216
Joe Rosseljong <general@veepzy.net>
2024-07-06 15:03:32 +0200

    chore: remove cookie types as they are now included

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,20 +2,10 @@ import "server-only";
 
 import { createServerClient } from "@supabase/ssr";
 import { createClient } from "@supabase/supabase-js";
-import type { CookieSerializeOptions } from "cookie";
 import { cookies } from "next/headers";
 import { type NextRequest, NextResponse } from "next/server";
 
 import { type Database } from "./schema";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import jwt from "jsonwebtoken";
-
-type CookieOptions = Partial<CookieSerializeOptions>;
-interface Cookies {
-    name: string;
-    value: string;
-    options: CookieOptions;
-}
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const SECRET_KEY = process.env.SUPABASE_SECRET_KEY ?? "";
@@ -38,7 +28,7 @@ export const createServerDBClient = () => {
             getAll() {
                 return cookieStore.getAll();
             },
-            setAll(cookiesToSet: Cookies[]) {
+            setAll(cookiesToSet) {
                 try {
                     cookiesToSet.forEach(({ name, value, options }) =>
                         cookieStore.set({ name, value, ...options }),
@@ -56,28 +46,24 @@ export const updateSession = async (request: NextRequest) => {
         },
     });
 
-    const db = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
-        {
-            cookies: {
-                getAll() {
-                    return request.cookies.getAll();
-                },
-                setAll(cookiesToSet: Cookies[]) {
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        request.cookies.set(name, value),
-                    );
-                    supabaseResponse = NextResponse.next({
-                        request,
-                    });
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        supabaseResponse.cookies.set(name, value, options),
-                    );
-                },
+    const db = createServerClient(SUPABASE_URL, ANON_KEY, {
+        cookies: {
+            getAll() {
+                return request.cookies.getAll();
+            },
+            setAll(cookiesToSet) {
+                cookiesToSet.forEach(({ name, value, options }) =>
+                    request.cookies.set(name, value),
+                );
+                supabaseResponse = NextResponse.next({
+                    request,
+                });
+                cookiesToSet.forEach(({ name, value, options }) =>
+                    supabaseResponse.cookies.set(name, value, options),
+                );
             },
         },
-    );
+    });
 
     await db.auth.getUser();
 
