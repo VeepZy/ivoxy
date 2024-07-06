a06efd79789ceba046ec184193efce3ae201e208
Joe Rosseljong <general@veepzy.net>
2024-07-06 16:50:48 +0200

    style: change svg to lucide icons, increase bottom padding and add more links to active class

--- src/features/sidebar/components/sidebar.tsx
+++ src/features/sidebar/components/sidebar.tsx
@@ -7,6 +7,12 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { type Database } from "@/db/schema";
 import { cn } from "@/lib/utils";
+import {
+    Grid2X2Icon,
+    LayoutGridIcon,
+    ListMusicIcon,
+    Music2Icon,
+} from "lucide-react";
 
 type Playlist = Database["public"]["Tables"]["playlists"]["Row"];
 
@@ -14,7 +20,7 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
     const pathname = usePathname();
 
     return (
-        <div className="hidden pb-12 lg:block">
+        <div className="hidden pb-20 lg:block">
             <div className="space-y-4 py-4">
                 <div className="px-3 py-2">
                     <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
@@ -44,50 +50,12 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                             asChild
                             variant="ghost"
                             className={cn("w-full justify-start", {
-                                "text-destructive hover:text-destructive":
+                                "text-primary hover:text-primary":
                                     pathname === "/browse",
                             })}
                         >
                             <Link href="/browse">
-                                <svg
-                                    className="mr-2 h-4 w-4"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <rect
-                                        height="7"
-                                        rx="1"
-                                        width="7"
-                                        x="3"
-                                        y="3"
-                                    />
-                                    <rect
-                                        height="7"
-                                        rx="1"
-                                        width="7"
-                                        x="14"
-                                        y="3"
-                                    />
-                                    <rect
-                                        height="7"
-                                        rx="1"
-                                        width="7"
-                                        x="14"
-                                        y="14"
-                                    />
-                                    <rect
-                                        height="7"
-                                        rx="1"
-                                        width="7"
-                                        x="3"
-                                        y="14"
-                                    />
-                                </svg>
+                                <LayoutGridIcon className="mr-2 mt-0.5 h-4 w-4" />
                                 Browse
                             </Link>
                         </Button>
@@ -122,106 +90,29 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                     <div className="space-y-1">
                         <Button
                             asChild
-                            className="w-full justify-start"
+                            className={cn("w-full justify-start", {
+                                "text-primary hover:text-primary":
+                                    pathname === "/playlists",
+                            })}
                             variant="ghost"
                         >
                             <Link href="/playlists">
-                                <svg
-                                    className="mr-2 h-4 w-4"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M21 15V6" />
-                                    <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                                    <path d="M12 12H3" />
-                                    <path d="M16 6H3" />
-                                    <path d="M12 18H3" />
-                                </svg>
+                                <ListMusicIcon className="mr-2 mt-0.5 h-4 w-4" />
                                 Playlists
                             </Link>
                         </Button>
                         <Button
-                            className="w-full justify-start"
+                            className={cn("w-full justify-start", {
+                                "text-primary hover:text-primary":
+                                    pathname === "/songs",
+                            })}
                             variant="ghost"
+                            asChild
                         >
-                            <svg
-                                className="mr-2 h-4 w-4"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <circle cx="8" cy="18" r="4" />
-                                <path d="M12 18V2l7 4" />
-                            </svg>
-                            Songs
-                        </Button>
-                        <Button
-                            className="w-full justify-start"
-                            variant="ghost"
-                        >
-                            <svg
-                                className="mr-2 h-4 w-4"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                                <circle cx="12" cy="7" r="4" />
-                            </svg>
-                            Made for You
-                        </Button>
-                        <Button
-                            className="w-full justify-start"
-                            variant="ghost"
-                        >
-                            <svg
-                                className="mr-2 h-4 w-4"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="m12 8-9.04 9.06a2.82 2.82 0 1 0 3.98 3.98L16 12" />
-                                <circle cx="17" cy="7" r="5" />
-                            </svg>
-                            Artists
-                        </Button>
-                        <Button
-                            className="w-full justify-start"
-                            variant="ghost"
-                        >
-                            <svg
-                                className="mr-2 h-4 w-4"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="m16 6 4 14" />
-                                <path d="M12 6v14" />
-                                <path d="M8 8v12" />
-                                <path d="M4 4v16" />
-                            </svg>
-                            Albums
+                            <Link href="/songs">
+                                <Music2Icon className="mr-2 mt-0.5 h-4 w-4" />
+                                Songs
+                            </Link>
                         </Button>
                     </div>
                 </div>
@@ -234,29 +125,21 @@ const Sidebar: React.FC<{ playlists: Playlist[] }> = ({ playlists }) => {
                             {playlists.map((playlist) => (
                                 <Button
                                     key={playlist.id}
-                                    className="w-full justify-start font-normal"
+                                    className={cn(
+                                        "w-full justify-start font-normal",
+                                        {
+                                            "text-primary hover:text-primary":
+                                                pathname ===
+                                                `/playlists/${playlist.id}`,
+                                        },
+                                    )}
                                     variant="ghost"
                                     asChild
                                 >
                                     <Link
                                         href={`/playlists/${playlist.id}`}
                                     >
-                                        <svg
-                                            className="mr-2 h-4 w-4"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            viewBox="0 0 24 24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path d="M21 15V6" />
-                                            <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                                            <path d="M12 12H3" />
-                                            <path d="M16 6H3" />
-                                            <path d="M12 18H3" />
-                                        </svg>
+                                        <ListMusicIcon className="mr-2 mt-0.5 h-4 w-4" />
                                         {playlist.name}
                                     </Link>
                                 </Button>
