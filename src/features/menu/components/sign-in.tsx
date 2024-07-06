395197c7bd65b16fad69f152e19cb9b5cf633b43
Joe Rosseljong <general@veepzy.net>
2024-07-06 15:04:08 +0200

    feat: render button if item is false

--- src/features/menu/components/sign-in.tsx
+++ src/features/menu/components/sign-in.tsx
@@ -3,8 +3,13 @@
 import { MenubarItem } from "@/components/ui/menubar";
 
 import { signIn } from "../api/sign-in";
+import { Button } from "@/components/ui/button";
 
-const SignIn: React.FC = () => {
+const SignIn: React.FC<{ item?: boolean }> = ({ item = false }) => {
+    if (!item) {
+        return <Button onClick={() => signIn()}>Sign In</Button>;
+    }
+
     return <MenubarItem onClick={() => signIn()}>Sign In</MenubarItem>;
 };
 
