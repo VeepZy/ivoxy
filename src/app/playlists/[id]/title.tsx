7462fa3031a8ca763d19afefb2f49c24bfc163d6
Joe Rosseljong <general@veepzy.net>
2024-07-06 16:48:34 +0200

    fix: use mounted for domparser

--- src/app/playlists/[id]/title.tsx
+++ src/app/playlists/[id]/title.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import { useMounted } from "@/hooks/mounted";
 import { unescapeHTML } from "@/lib/utils.client";
 
 const PlaylistTitle: React.FC<{ title: string }> = ({ title }) => {
+    const mounted = useMounted();
+
+    if (!mounted) {
+        return null;
+    }
+
     return (
         <h3 className="font-semibold leading-none">
             {unescapeHTML(title)}
