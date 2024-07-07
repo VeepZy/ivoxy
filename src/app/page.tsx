150a2c773e0ea8f40b57f8cc89cd6404b7be986b
Joe Rosseljong <general@veepzy.net>
2024-07-07 13:36:30 +0200

    chore: formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { type NextPage } from "next";
 const HomeRoute: NextPage = () => {
     return (
         <div className="h-full px-4 py-6 lg:px-8">
-                <p>Home</p>
-            </div>
+            <p>Home</p>
+        </div>
     );
 };
 
