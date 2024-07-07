c6fe0c78f84807de3adf45e78bfb6bdd9a162ce1
Joe Rosseljong <general@veepzy.net>
2024-07-07 18:50:10 +0200

    feat: add play button to songs page

--- src/app/songs/page.tsx
+++ src/app/songs/page.tsx
@@ -1,9 +1,9 @@
-import { PlayIcon } from "lucide-react";
 import { type NextPage } from "next";
 import Image from "next/image";
 
 import { Title } from "@/components/title";
 import { getSongs } from "@/db/queries";
+import { SongPlayButton } from "@/components/play";
 
 const SongsRoute: NextPage = async () => {
     const songs = await getSongs();
@@ -24,7 +24,7 @@ const SongsRoute: NextPage = async () => {
                         key={item.data.title}
                         className="w-[320px] space-y-3"
                     >
-                        <div className="relative overflow-hidden rounded-md border border-primary/50 shadow-lg">
+                        <div className="group relative overflow-hidden rounded-md border border-primary/50 shadow-lg">
                             <Image
                                 alt={item.data.title}
                                 className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
@@ -32,8 +32,8 @@ const SongsRoute: NextPage = async () => {
                                 src={item.data.thumbnail}
                                 width={320}
                             />
-                            <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:opacity-100">
-                                <PlayIcon className="h-10 w-10 text-white" />
+                            <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:cursor-pointer group-hover:opacity-100">
+                                <SongPlayButton song={item.data} />
                             </div>
                         </div>
                         <div className="space-y-1 text-sm">
