5a66ece3af34ebaed2b4d51b9353c5f8b614a27c
Joe Rosseljong <general@veepzy.net>
2024-07-07 18:41:18 +0200

    feat: update progress bar to support loaded state bar

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,10 +5,14 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+type ProgressSegment = { value: number; load?: boolean };
+
 const Progress = React.forwardRef<
     React.ElementRef<typeof ProgressPrimitive.Root>,
-    React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, max, ...props }, ref) => (
+    React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+        segments: ProgressSegment[];
+    }
+>(({ className, segments, max, ...props }, ref) => (
     <ProgressPrimitive.Root
         ref={ref}
         className={cn(
@@ -17,14 +21,22 @@ const Progress = React.forwardRef<
         )}
         {...props}
     >
-        <ProgressPrimitive.Indicator
-            className="h-full w-full flex-1 bg-primary transition-all"
-            style={{
-                transform: `translateX(-${100 - ((value || 0) / (max || 100)) * 100}%)`,
-            }}
-        />
+        {segments.map((segment, index) => (
+            <ProgressPrimitive.Indicator
+                key={index}
+                className={cn(
+                    "absolute h-full w-full transition-all",
+                    segment.load ? `bg-primary/20` : "bg-primary",
+                )}
+                style={{
+                    transform: `translateX(-${100 - ((segment.value || 0) / (max || 100)) * 100}%)`,
+                    zIndex: segments.length - index,
+                }}
+            />
+        ))}
     </ProgressPrimitive.Root>
 ));
+
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
