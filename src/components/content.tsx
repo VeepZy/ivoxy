ace71d874ad13612127825e982956c28978272bc
Joe Rosseljong <general@veepzy.net>
2024-07-08 15:22:50 +0200

    feat: server component for content

---
+++ src/components/content.tsx
@@ -0,0 +1,14 @@
+import { getPlaylists } from "@/db/queries";
+import { ContentWrapper } from "./content-wrapper";
+
+const Content: React.FC<{ children: React.ReactNode }> = async ({
+    children,
+}) => {
+    const playlists = await getPlaylists();
+
+    return (
+        <ContentWrapper playlists={playlists}>{children}</ContentWrapper>
+    );
+};
+
+export { Content };
