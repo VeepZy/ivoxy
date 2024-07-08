3620ce111bb90339e262c922ca98ab1243a8b35a
Joe Rosseljong <general@veepzy.net>
2024-07-08 20:52:59 +0200

    refactor: use new components that handle playing and setting the title

--- src/features/search/components/items.tsx
+++ src/features/search/components/items.tsx
@@ -1,67 +1,52 @@
 "use client";
 
 import { type youtube_v3 as Youtube } from "googleapis";
-import { PlayIcon } from "lucide-react";
 import Image from "next/image";
-import { useContext } from "react";
-
-import { PlayerContext } from "@/features/player/components/context";
-import { useMounted } from "@/hooks/mounted";
-import { unescapeHTML } from "@/lib/utils.client";
+import { SongPlayButton } from "@/components/play";
+import { Title } from "@/components/title";
+import { BrowseMoreButton } from "@/app/browse/more";
 
 const Items: React.FC<{ items: Youtube.Schema$SearchResult[] }> = ({
     items,
 }) => {
-    const { setUrl } = useContext(PlayerContext);
-
-    const isMounted = useMounted();
-
-    if (!isMounted) {
-        return null;
-    }
-
-    const onSubmit = (item: Youtube.Schema$SearchResult) => {
-        if (!item.id?.videoId) return;
-
-        setUrl([
-            {
-                title: item.snippet?.title ?? "",
-                channelTitle: item.snippet?.channelTitle ?? "",
-                url: `https://www.youtube.com/embed/${item.id.videoId}`,
-                thumbnail: item.snippet?.thumbnails?.medium?.url ?? "",
-            },
-        ]);
-    };
-
     return (
         <div className="flex w-full flex-row flex-wrap gap-8">
             {items.map((item) => (
                 <div key={item.etag} className="w-[320px] space-y-3">
-                    <button
-                        className="relative overflow-hidden rounded-md border border-primary/50 shadow-lg"
-                        type="button"
-                        onClick={() => onSubmit(item)}
-                    >
+                    <div className="group relative overflow-hidden rounded-md border border-primary/50 shadow-lg">
                         <Image
                             alt={item.snippet?.title ?? ""}
                             className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
                             height={180}
-                            width={320}
                             src={
                                 item.snippet?.thumbnails?.medium?.url ?? ""
                             }
+                            width={320}
                         />
-                        <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:opacity-100">
-                            <PlayIcon className="h-10 w-10 text-white" />
+
+                        <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:cursor-pointer group-hover:opacity-100">
+                            <SongPlayButton
+                                song={{
+                                    title: item.snippet?.title ?? "",
+                                    channelTitle:
+                                        item.snippet?.channelTitle ?? "",
+                                    url: `https://www.youtube.com/embed/${item.id?.videoId ?? ""}`,
+                                    thumbnail:
+                                        item.snippet?.thumbnails?.medium
+                                            ?.url ?? "",
+                                }}
+                            />
                         </div>
-                    </button>
-                    <div className="space-y-1 text-sm">
-                        <h3 className="font-semibold leading-none">
-                            {unescapeHTML(item.snippet?.title ?? "")}
-                        </h3>
-                        <p className="text-xs text-muted-foreground">
-                            {item.snippet?.channelTitle ?? ""}
-                        </p>
+                    </div>
+
+                    <div className="flex items-center">
+                        <div className="flex-1 space-y-1 text-sm">
+                            <Title title={item.snippet?.title ?? ""} />
+                            <p className="text-xs text-muted-foreground">
+                                {item.snippet?.channelTitle ?? ""}
+                            </p>
+                        </div>
+                        <BrowseMoreButton song={item} />
                     </div>
                 </div>
             ))}
