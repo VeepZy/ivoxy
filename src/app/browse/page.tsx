597724932ec6c8854e6e7864342f31d278e46713
Joe Rosseljong <general@veepzy.net>
2024-07-09 16:53:57 +0200

    feat: check if google access token exists

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -4,8 +4,16 @@ import type { NextPage } from "next";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Search } from "@/features/search/components/search";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const BrowseRoute: NextPage = () => {
+    const cookieStore = cookies();
+
+    if (!cookieStore.get("google_access_token")) {
+        redirect("/profile");
+    }
+
     return (
         <ScrollArea className="max-h-full overflow-y-auto">
             <Search />
