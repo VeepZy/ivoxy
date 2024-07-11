18429428c3e8969c5dfce0cd186ac7337005ddd1
Joe Rosseljong <general@veepzy.net>
2024-07-11 21:32:23 +0200

    feat: more button for songs

---
+++ src/components/song-more/more.tsx
@@ -0,0 +1,49 @@
+import { MoreVerticalIcon } from "lucide-react";
+
+import { type Playlist, type Song } from "@/db/types";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuSub,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+import { AddToPlaylist } from "./playlist-add";
+import { SongRemoveButton } from "./remove";
+
+const SongMoreButton: React.FC<{ playlists: Playlist[]; song: Song }> = ({
+    song,
+    playlists,
+}) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <MoreVerticalIcon className="h-4 w-4" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>Settings</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>
+                        Add To Playlist
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                        <AddToPlaylist playlists={playlists} song={song} />
+                    </DropdownMenuPortal>
+                </DropdownMenuSub>
+                <DropdownMenuSeparator />
+                <SongRemoveButton song={song} />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+export { SongMoreButton };
