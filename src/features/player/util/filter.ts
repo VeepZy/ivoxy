d186f4a10ab86caa8d999bf5982b85f3794ad6ff
Joe Rosseljong <general@veepzy.net>
2024-07-11 21:34:15 +0200

    feat: filter songs

--- src/features/player/util/filter.ts
+++ src/features/player/util/filter.ts
@@ -1,7 +1,11 @@
-import { Playlist } from "@/db/types";
+import { type Playlist, type Song } from "@/db/types";
 
 export const filter = (playlists: Playlist[], current: string) => {
     return playlists.filter(
         (p) => !p.data.some((item) => item.url === current),
     );
+};
+
+export const filterSongs = (songs: Song[], current: string) => {
+    return songs.some((p) => p.data.url !== current);
 };
