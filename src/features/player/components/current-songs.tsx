df3cc14bdbdcc04feee8e87b9e586f08ced9b7d2
Joe Rosseljong <general@veepzy.net>
2024-07-14 18:56:41 +0200

    refactor: use new player store state

--- src/features/player/components/current-songs.tsx
+++ src/features/player/components/current-songs.tsx
@@ -11,32 +11,35 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { setIndex, usePlayerStore } from "@/hooks/player";
+import { cn } from "@/lib/utils";
 
-import { usePlayerStore } from "@/hooks/player";
+import { AudioLines } from "./audio-lines";
 
 const CurrentSongs: React.FC = () => {
-    const state = usePlayerStore((store) => store.state);
-    const setIndex = usePlayerStore((store) => store.control.setIndex);
+    const data = usePlayerStore((store) => store.data);
+    const songIndex = usePlayerStore((store) => store.index);
+    const playing = usePlayerStore((store) => store.playing);
 
     return (
         <Popover>
             <PopoverTrigger asChild>
-                <Button
-                    disabled={state.data.length === 1}
-                    variant="outline"
-                >
+                <Button disabled={data?.length === 1} variant="outline">
                     <ListMusicIcon className="h-5 w-5" />
                 </Button>
             </PopoverTrigger>
-            <PopoverContent align="end" className="w-80">
+            <PopoverContent align="end" className="w-80 p-0">
                 <ScrollArea className="max-h-96">
                     <ul className="space-y-2">
-                        {state.data.map((item, index) => (
+                        {data?.map((item, index) => (
                             <li
                                 key={item.title}
-                                className="grid grid-cols-[40px_1fr_auto] items-center gap-4"
+                                className={cn(
+                                    "grid grid-cols-[40px_1fr_auto] items-center gap-4 p-2",
+                                    songIndex === index && "bg-secondary",
+                                )}
                             >
-                                <div className="relative overflow-hidden rounded-md border border-primary/50 shadow-lg dark:border-primary/30">
+                                <div className="relative overflow-hidden rounded-md border border-primary/50  shadow-lg dark:border-primary/30">
                                     <Image
                                         alt={item.title}
                                         className="aspect-square object-cover transition-all hover:scale-105"
@@ -47,7 +50,7 @@ const CurrentSongs: React.FC = () => {
                                 </div>
                                 <div className="grid">
                                     <Title
-                                        className="text-sm"
+                                        className="text-xs"
                                         title={item.title}
                                     />
                                     <p className="text-xs text-muted-foreground">
@@ -60,7 +63,11 @@ const CurrentSongs: React.FC = () => {
                                         variant="ghost"
                                         onClick={() => setIndex(index)}
                                     >
-                                        <PlayIcon className="h-4 w-4" />
+                                        {songIndex === index && playing ? (
+                                            <AudioLines className="h-4 w-4" />
+                                        ) : (
+                                            <PlayIcon className="h-4 w-4" />
+                                        )}
                                     </Button>
                                 </div>
                             </li>
