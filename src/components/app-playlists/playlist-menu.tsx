a8a3a7a204d8645d9d8ae73584264585506c6002
Joe Rosseljong <general@veepzy.net>
2024-07-16 18:43:19 +0200

    feat: playlist menu

---
+++ src/components/app-playlists/playlist-menu.tsx
@@ -0,0 +1,54 @@
+import {
+    ListCollapseIcon,
+    ListMinusIcon,
+    MoreVerticalIcon,
+} from "lucide-react";
+import Link from "next/link";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { type Playlist } from "@/db/types";
+
+import { Dialog, DialogTrigger } from "../ui/dialog";
+
+import { DropdownMenuAddToCurrent } from "./playlist-menu-current";
+import { DropdownMenuRemove } from "./playlist-menu-remove";
+import { DropdownMenuDialogRename } from "./playlist-menu-rename";
+
+const PlaylistMenu: React.FC<{ playlist: Playlist }> = ({ playlist }) => {
+    return (
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <MoreVerticalIcon className="h-4 w-4" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-72">
+                    <DropdownMenuItem asChild>
+                        <Link href={`/playlists/${playlist.id}`}>
+                            <ListCollapseIcon className="mr-2 h-5 w-5" />
+                            View
+                        </Link>
+                    </DropdownMenuItem>
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>
+                            <ListMinusIcon className="mr-2 h-5 w-5" />
+                            Rename
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                    <DropdownMenuAddToCurrent playlist={playlist} />
+
+                    <DropdownMenuSeparator />
+                    <DropdownMenuRemove playlist={playlist} />
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenuDialogRename playlist={playlist} />
+        </Dialog>
+    );
+};
+
+export { PlaylistMenu };
