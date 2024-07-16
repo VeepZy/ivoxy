d516b1daaa99d21ac50be565cbbdd9bed62639ac
Joe Rosseljong <general@veepzy.net>
2024-07-16 18:45:58 +0200

    refactor: replace with containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,107 +1,16 @@
 import { type NextPage } from "next";
-import Image from "next/image";
 
-import {
-    PlayPlaylistButton,
-    PlaySongButton,
-} from "@/components/play-buttons";
-import { Title } from "@/components/title";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { getPlaylists, getSongs } from "@/db/queries";
-import { type Playlist, type Song } from "@/db/types";
-import { SongMoreButton } from "@/components/song-more/more";
-import { PlaylistMoreButton } from "@/components/playlist-more/more";
 
-type CombinedItem = Song | Playlist;
+import { SongsContainer } from "@/components/app-songs/container";
+import { PlaylistContainer } from "@/components/app-playlists/container";
 
-const HomeRoute: NextPage = async () => {
-    const [songs, playlists] = await Promise.all([
-        getSongs(),
-        getPlaylists(),
-    ]);
-
-    const combined = [...songs, ...playlists];
-
-    const isSong = (item: CombinedItem): item is Song =>
-        !Array.isArray(item.data);
-
+const HomeRoute: NextPage = () => {
     return (
         <ScrollArea className="max-h-full overflow-y-auto p-6">
             <div className="flex w-full flex-row flex-wrap gap-8">
-                {combined.map((item) =>
-                    isSong(item) ? (
-                        <div key={item.id} className="w-[320px] space-y-3">
-                            <div className="group relative overflow-hidden rounded-md border border-primary/50 shadow-lg">
-                                <Image
-                                    alt={item.data.title}
-                                    className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
-                                    height={180}
-                                    src={item.data.thumbnail}
-                                    width={320}
-                                />
-
-                                <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:cursor-pointer group-hover:opacity-100">
-                                    <PlaySongButton
-                                        song={{
-                                            title: item.data.title,
-                                            channelTitle:
-                                                item.data.channelTitle,
-                                            url: `https://www.youtube.com/embed/${item.data.url}`,
-                                            thumbnail: item.data.thumbnail,
-                                        }}
-                                    />
-                                </div>
-                            </div>
-
-                            <div className="flex items-center">
-                                <div className="flex-1 space-y-1 text-sm">
-                                    <Title title={item.data.title} />
-                                    <p className="text-xs text-muted-foreground">
-                                        {item.data.channelTitle}
-                                    </p>
-                                </div>
-                                <SongMoreButton
-                                    song={item}
-                                    playlists={playlists}
-                                />
-                            </div>
-                        </div>
-                    ) : (
-                        <div key={item.id} className="w-[320px] space-y-3">
-                            <div className="group relative overflow-hidden rounded-md border border-primary/50 shadow-lg">
-                                <Image
-                                    alt={item.name}
-                                    className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
-                                    height={180}
-                                    width={320}
-                                    src={
-                                        item.image_src ??
-                                        "/thumbnail-placeholder.jpg"
-                                    }
-                                />
-
-                                <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:cursor-pointer group-hover:opacity-100">
-                                    <PlayPlaylistButton
-                                        playlist={item.data}
-                                    />
-                                </div>
-                            </div>
-
-                            <div className="flex items-center">
-                                <div className="flex-1 space-y-1 text-sm">
-                                    <Title title={item.name} />
-                                    <p className="text-xs text-muted-foreground">
-                                        {item.data.length} songs
-                                    </p>
-                                </div>
-                                <PlaylistMoreButton
-                                    playlist={item}
-                                    playlists={playlists}
-                                />
-                            </div>
-                        </div>
-                    ),
-                )}
+                <SongsContainer />
+                <PlaylistContainer />
             </div>
         </ScrollArea>
     );
