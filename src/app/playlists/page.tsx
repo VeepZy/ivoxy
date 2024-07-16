8d708ce23c3409a5be130f1d43b21963c7cf46b0
Joe Rosseljong <general@veepzy.net>
2024-07-16 18:46:09 +0200

    refactor: replace with container

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,53 +1,13 @@
 import { type NextPage } from "next";
-import Image from "next/image";
 
-import { PlaylistMoreButton } from "@/components//playlist-more/more";
-import { PlayPlaylistButton } from "@/components/play-buttons";
-import { Title } from "@/components/title";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { getPlaylists } from "@/db/queries";
+import { PlaylistContainer } from "@/components/app-playlists/container";
 
-const PlaylistsRoute: NextPage = async () => {
-    const playlists = await getPlaylists();
-
+const PlaylistsRoute: NextPage = () => {
     return (
         <ScrollArea className="max-h-full overflow-y-auto p-6">
             <div className="flex w-full flex-row flex-wrap gap-8">
-                {playlists.map((playlist) => (
-                    <div key={playlist.id} className="w-[320px] space-y-3">
-                        <div className="group relative overflow-hidden rounded-md border border-primary/50 shadow-lg">
-                            <Image
-                                alt={playlist.name}
-                                className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
-                                height={180}
-                                width={320}
-                                src={
-                                    playlist.image_src ??
-                                    "/thumbnail-placeholder.jpg"
-                                }
-                            />
-
-                            <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:cursor-pointer group-hover:opacity-100">
-                                <PlayPlaylistButton
-                                    playlist={playlist.data}
-                                />
-                            </div>
-                        </div>
-
-                        <div className="flex items-center">
-                            <div className="flex-1 space-y-1 text-sm">
-                                <Title title={playlist.name} />
-                                <p className="text-xs text-muted-foreground">
-                                    {playlist.data.length} songs
-                                </p>
-                            </div>
-                            <PlaylistMoreButton
-                                playlist={playlist}
-                                playlists={playlists}
-                            />
-                        </div>
-                    </div>
-                ))}
+                <PlaylistContainer />
             </div>
         </ScrollArea>
     );
