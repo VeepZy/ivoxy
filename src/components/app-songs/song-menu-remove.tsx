186b62b8fa9d48a689907001f493db0c68b49b16
Joe Rosseljong <general@veepzy.net>
2024-07-16 18:42:01 +0200

    feat: song menu remove button

---
+++ src/components/app-songs/song-menu-remove.tsx
@@ -0,0 +1,31 @@
+import { ClockArrowUpIcon } from "lucide-react";
+
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { removeSong } from "@/db/actions";
+import { type Song } from "@/db/types";
+import { useRemove } from "@/hooks/use-remove";
+
+const DropdownMenuRemove: React.FC<{ song: Song }> = ({ song }) => {
+    const [fill, pending, startTimeout, stopTimeout] = useRemove(() =>
+        removeSong(song),
+    );
+
+    return (
+        <DropdownMenuItem
+            className="relative flex justify-between text-destructive hover:cursor-pointer hover:bg-secondary"
+            disabled={pending}
+            onMouseDown={startTimeout}
+            onMouseLeave={stopTimeout}
+            onMouseUp={stopTimeout}
+        >
+            <div
+                className="absolute bottom-0 left-0 top-0 z-10 rounded-sm bg-primary/50"
+                style={{ width: `${fill}%` }}
+            />
+            Remove
+            <ClockArrowUpIcon className="h-4 w-4" />
+        </DropdownMenuItem>
+    );
+};
+
+export { DropdownMenuRemove };
