e0db6f8cdac0493ec00406f54c4e59e4b8c80941
Joe Rosseljong <general@veepzy.net>
2024-07-17 17:01:55 +0200

    feat: app search item display

---
+++ src/features/app-search/components/items.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { LoaderCircleIcon } from "lucide-react";
+
+import { Song } from "@/components/app-songs/song";
+import { Button } from "@/components/ui/button";
+
+import { useSearchStore } from "../stores/items";
+
+const AppSearchItems: React.FC = () => {
+    const items = useSearchStore((state) => state.items);
+    const loadMore = useSearchStore((state) => state.loadMore);
+    const setLoadMore = useSearchStore((state) => state.setLoadMore);
+
+    return (
+        <>
+            <div className="flex w-full flex-row flex-wrap gap-8">
+                {items.map((item, index) => (
+                    <Song key={`${item.url}-${index}`} song={item} />
+                ))}
+            </div>
+            {items.length > 0 && (
+                <div className="mt-8 flex justify-center">
+                    <Button
+                        disabled={loadMore}
+                        onClick={() => setLoadMore(true)}
+                    >
+                        {loadMore ? (
+                            <LoaderCircleIcon className="h-4 w-4 animate-spin" />
+                        ) : (
+                            <>Load More</>
+                        )}
+                    </Button>
+                </div>
+            )}
+        </>
+    );
+};
+
+export { AppSearchItems };
