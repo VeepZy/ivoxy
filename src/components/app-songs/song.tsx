cfc0b5b83b4ade3a3221066d090051c16d5f9572
Joe Rosseljong <general@veepzy.net>
2024-07-17 17:01:19 +0200

    refator: support either song or songdata

--- src/components/app-songs/song.tsx
+++ src/components/app-songs/song.tsx
@@ -2,34 +2,37 @@
 
 import Image from "next/image";
 
-import type { Song } from "@/db/types";
+import type { Song, SongData } from "@/db/types";
 import { UnescapedTitle } from "@/features/unescaped-title/components";
 
 import { SongMenu } from "./song-menu";
 import { PlaySongButton } from "./song-play";
 
-const Song: React.FC<{ song: Song }> = ({ song }) => {
+const Song: React.FC<{ song: Song | SongData }> = ({ song }) => {
+    const isSong = typeof song === "object" && "data" in song;
+    const data = isSong ? song.data : song;
+
     return (
         <div className="w-[320px] space-y-3">
             <div className="group relative overflow-hidden rounded-md border-2 border-foreground drop-shadow-md">
                 <Image
-                    alt={song.data.title}
+                    alt={data.title}
                     className="aspect-square h-[180px] w-[320px] object-cover transition-all hover:scale-105"
                     height={180}
-                    src={song.data.thumbnail}
+                    src={data.thumbnail}
                     width={320}
                 />
 
                 <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-all group-hover:cursor-pointer group-hover:opacity-100">
-                    <PlaySongButton song={song.data} />
+                    <PlaySongButton song={data} />
                 </div>
             </div>
 
             <div className="flex items-center">
                 <div className="flex-1 space-y-1 text-sm">
-                    <UnescapedTitle title={song.data.title} />
+                    <UnescapedTitle title={data.title} />
                     <p className="text-xs text-muted-foreground">
-                        {song.data.channelTitle}
+                        {data.channelTitle}
                     </p>
                 </div>
                 <SongMenu song={song} />
