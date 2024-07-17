bf3d8fcebe7f7e950dc67e5979641e3e6cdb8576
Joe Rosseljong <general@veepzy.net>
2024-07-17 17:00:23 +0200

    feat: support either Song or SongData

--- src/components/app-songs/song-menu.tsx
+++ src/components/app-songs/song-menu.tsx
@@ -6,7 +6,7 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { type Song } from "@/db/types";
+import { type Song, type SongData } from "@/db/types";
 import { useDataStore } from "@/hooks/use-data";
 import { isSavedSong } from "@/utils/filter";
 
@@ -14,10 +14,13 @@ import { DropdownMenuAddToCurrent } from "./song-menu-current";
 import { DropdownMenuAddToPlaylist } from "./song-menu-playlist";
 import { DropdownMenuRemove } from "./song-menu-remove";
 
-const SongMenu: React.FC<{ song: Song }> = ({ song }) => {
+const SongMenu: React.FC<{ song: Song | SongData }> = ({ song }) => {
     const { songs } = useDataStore();
 
-    const isSaved = isSavedSong(songs, song.data.url);
+    const isSong = typeof song === "object" && "data" in song;
+    const data = isSong ? song.data : song;
+
+    const isSaved = isSong ? isSavedSong(songs, data.url) : false;
 
     return (
         <DropdownMenu>
@@ -25,12 +28,14 @@ const SongMenu: React.FC<{ song: Song }> = ({ song }) => {
                 <MoreVerticalIcon className="h-4 w-4" />
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-72">
-                <DropdownMenuAddToCurrent song={song} />
-                <DropdownMenuAddToPlaylist song={song} />
-                {isSaved ? <>
+                <DropdownMenuAddToCurrent song={data} />
+                <DropdownMenuAddToPlaylist song={data} />
+                {isSaved ? (
+                    <>
                         <DropdownMenuSeparator />
-                        <DropdownMenuRemove song={song} />
-                    </> : null}
+                        <DropdownMenuRemove song={song as Song} />
+                    </>
+                ) : null}
             </DropdownMenuContent>
         </DropdownMenu>
     );
