320123ef521348ecba9afc3051f76355d13eca40
Joe Rosseljong <general@veepzy.net>
2024-07-18 19:16:19 +0200

    feat: add logitc to progress bar

--- src/features/app-player/components/player-progress.tsx
+++ src/features/app-player/components/player-progress.tsx
@@ -2,16 +2,33 @@
 
 import { Progress } from "@/components/ui/progress";
 import { PlayerRef } from "../types/player-ref";
+import { usePlayerStore } from "@/stores/player";
+import { MouseEvent, useMemo } from "react";
 
 const AppPlayerProgress: React.FC<{ player: PlayerRef }> = ({
     player,
 }) => {
+    const played = usePlayerStore((store) => store.played);
+    const loaded = usePlayerStore((store) => store.loaded);
+    const duration = usePlayerStore((store) => store.duration);
+
+    const seekTo = useMemo(
+        () => (event: MouseEvent<HTMLDivElement>) => {
+            const x = event.clientX;
+            const width = event.currentTarget.clientWidth;
+            const progress = Math.floor((x / width) * duration);
+
+            player.current?.seekTo(progress, "seconds");
+        },
+        [player, duration],
+    );
+
     return (
         <Progress
             className="h-2 rounded-none border-none"
-            max={100}
-            segments={[{ value: 50 }, { value: 75, load: true }]}
-            onClick={(event) => void 0}
+            max={duration}
+            onClick={seekTo}
+            segments={[{ value: played }, { value: loaded, load: true }]}
         />
     );
 };
