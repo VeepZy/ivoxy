89b9e50e6aea78bf5ce079c744879569f0913d65
Joe Rosseljong <general@veepzy.net>
2024-07-18 19:16:39 +0200

    feat: add logic to duration

--- src/features/app-player/components/player-duration.tsx
+++ src/features/app-player/components/player-duration.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { usePlayerStore } from "@/stores/player";
+import { formatDuration } from "../utils/format-duration";
+import { useMemo } from "react";
+
 const AppPlayerDuration: React.FC = () => {
-    return <p>{`${"00"}:${"00"}`}</p>;
+    const played = usePlayerStore((store) => store.played);
+    const duration = usePlayerStore((store) => store.duration);
+
+    const formattedDuration = useMemo(
+        () => formatDuration(duration),
+        [duration],
+    );
+
+    return <p>{`${formatDuration(played)}:${formattedDuration}`}</p>;
 };
 
 export { AppPlayerDuration };
