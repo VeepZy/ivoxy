424f379233937c814f416fb62213384dc3603aa7
Joe Rosseljong <general@veepzy.net>
2024-07-18 19:17:14 +0200

    feat: add logic to play button

--- src/components/app-songs/song-play.tsx
+++ src/components/app-songs/song-play.tsx
@@ -1,14 +1,20 @@
+"use client";
+
 import { PlayIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { type SongData } from "@/db/types";
+import { usePlayerStore } from "@/stores/player";
 
 const PlaySongButton: React.FC<{ song: SongData }> = ({ song }) => {
+    const setData = usePlayerStore((store) => store.setData);
+
     return (
         <Button
             className="group-hover:bg-primary"
             size="icon"
             variant="ghost"
+            onClick={() => setData([song])}
         >
             <span className="sr-only">Play</span>
             <PlayIcon className="h-5 w-5" />
