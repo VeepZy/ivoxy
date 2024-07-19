2f2f6a5c4ad18151970902abee1bd956752716be
Joe Rosseljong <general@veepzy.net>
2024-07-19 19:10:30 +0200

    chore: remove unused code

--- src/lib/utils.client.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-export const unescapeHTML = (html: string) => {
-    const parser = new DOMParser();
-    return (
-        parser.parseFromString(html, "text/html").body.textContent ?? ""
-    );
-};
