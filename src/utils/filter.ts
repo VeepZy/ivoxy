e376a0c7166f68ca4d7a767b0d0a7c4923e1b14a
Joe Rosseljong <general@veepzy.net>
2024-07-19 19:10:48 +0200

    feat: add support for inverse

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -3,10 +3,15 @@ import { Playlist, Song } from "@/db/types";
 export const filterPlaylists = (
     playlists: Playlist[],
     current: string,
+    inverse = false,
 ) => {
-    return playlists.filter(
-        (p) => !p.data.some((item) => item.url === current),
-    );
+    return playlists.filter((p) => {
+        if (inverse) {
+            return p.data.some((item) => item.url === current);
+        }
+
+        return !p.data.some((item) => item.url === current);
+    });
 };
 
 export const isSavedSong = (songs: Song[], current: string) => {
