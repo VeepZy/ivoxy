d6969be16962a898cb65d423e89ff60dc91c77aa
Joe Rosseljong <general@veepzy.net>
2024-07-19 21:25:17 +0200

    fix: onClick on non-interactive element

--- src/components/app-playlists/playlist-compact.tsx
+++ src/components/app-playlists/playlist-compact.tsx
@@ -4,12 +4,11 @@ import Image from "next/image";
 
 import { type SongData } from "@/db/types";
 import { UnescapedTitle } from "@/features/unescaped-title/components";
-
-import { PlaySongButton } from "../app-songs/song-play";
 import { usePlayerStore } from "@/stores/player";
-import { cn } from "@/lib/utils";
-import { AudioLinesIcon } from "../audio-icon";
+
 import { SongMenu } from "../app-songs/song-menu";
+import { AudioLinesIcon } from "../audio-icon";
+import { Button } from "../ui/button";
 
 const PlaylistCompact: React.FC<{ song: SongData }> = ({ song }) => {
     const data = usePlayerStore((store) => store.data);
@@ -20,40 +19,40 @@ const PlaylistCompact: React.FC<{ song: SongData }> = ({ song }) => {
     const isCurrentPlaying = isPlaying && data?.[index]?.url === song.url;
 
     return (
-        <li
-            onClick={() => setData([song])}
-            className={cn(
-                "grid grid-cols-[40px_1fr_auto] items-center gap-3 rounded-sm border px-3 py-2 hover:cursor-pointer hover:bg-secondary",
-                isCurrentPlaying
-                    ? "bg-secondary/50"
-                    : "border-transparent",
-            )}
-        >
-            <div className="relative h-10 w-10 overflow-hidden rounded-md border-2 border-foreground drop-shadow-md">
-                <Image
-                    alt={song.title}
-                    className="aspect-square object-cover transition-all hover:scale-105"
-                    height={120}
-                    src={song.thumbnail}
-                    width={320}
-                />
-            </div>
-            <div>
-                <UnescapedTitle
-                    title={song.title}
-                    className="text-xs tracking-tight"
-                />
-                <p className="text-xs text-muted-foreground">
-                    {song.channelTitle}
-                </p>
-            </div>
+        <li>
+            <Button
+                className="h-full w-full p-0"
+                variant={isCurrentPlaying ? "secondary" : "ghost"}
+                onClick={() => setData([song])}
+            >
+                <div className="grid w-full grid-cols-[40px_1fr_auto] items-center gap-3 rounded-sm px-3 py-2">
+                    <div className="relative h-10 w-10 overflow-hidden rounded-md border-2 border-foreground drop-shadow-md">
+                        <Image
+                            alt={song.title}
+                            className="aspect-square object-cover transition-all hover:scale-105"
+                            height={120}
+                            src={song.thumbnail}
+                            width={320}
+                        />
+                    </div>
+                    <div className="text-left">
+                        <UnescapedTitle
+                            className="text-xs tracking-tight"
+                            title={song.title}
+                        />
+                        <p className="text-xs text-muted-foreground">
+                            {song.channelTitle}
+                        </p>
+                    </div>
 
-            <div className="flex items-center gap-2">
-                {isCurrentPlaying && (
-                    <AudioLinesIcon className="h-5 w-5" />
-                )}
-                <SongMenu song={song} />
-            </div>
+                    <div className="flex items-center gap-2">
+                        {isCurrentPlaying ? (
+                            <AudioLinesIcon className="h-5 w-5" />
+                        ) : null}
+                        <SongMenu song={song} />
+                    </div>
+                </div>
+            </Button>
         </li>
     );
 };
--- src/features/app-player/components/player-mix-menu/mix-song.tsx
+++ src/features/app-player/components/player-mix-menu/mix-song.tsx
@@ -1,10 +1,13 @@
-import { SongData } from "@/db/types";
 import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { type SongData } from "@/db/types";
 import { UnescapedTitle } from "@/features/unescaped-title/components";
-import { AppPlayerMixSongMenu } from "./mix-song-menu";
-import { cn } from "@/lib/utils";
 import { usePlayerStore } from "@/stores/player";
+
 import { AudioLinesIcon } from "../../../../components/audio-icon";
+
+import { AppPlayerMixSongMenu } from "./mix-song-menu";
 
 const AppPlayerMixMenuSong: React.FC<{
     song: SongData;
@@ -19,41 +22,74 @@ const AppPlayerMixMenuSong: React.FC<{
     const isCurrentPlaying = isPlaying && index === currentIndex;
 
     return (
-        <li
-            onClick={() => setCurrentIndex(index)}
-            className={cn(
-                "grid grid-cols-[40px_1fr_auto] items-center gap-3 rounded-sm border px-3 py-2 hover:cursor-pointer hover:bg-secondary",
-                index === currentIndex
-                    ? "bg-secondary/50"
-                    : "border-transparent",
-            )}
-        >
-            <div className="relative h-10 w-10 overflow-hidden rounded-md border-2 border-foreground drop-shadow-md">
-                <Image
-                    alt={song.title}
-                    className="aspect-square object-cover transition-all hover:scale-105"
-                    height={120}
-                    src={song.thumbnail}
-                    width={320}
-                />
-            </div>
-            <div>
-                <UnescapedTitle
-                    title={song.title}
-                    className="text-xs tracking-tight"
-                />
-                <p className="text-xs text-muted-foreground">
-                    {song.channelTitle}
-                </p>
-            </div>
+        <li>
+            <Button
+                className="h-full w-full p-0"
+                variant={isCurrentPlaying ? "secondary" : "ghost"}
+                onClick={() => setCurrentIndex(index)}
+            >
+                <div className="grid w-full grid-cols-[40px_1fr_auto] items-center gap-3 rounded-sm px-3 py-2">
+                    <div className="relative h-10 w-10 overflow-hidden rounded-md border-2 border-foreground drop-shadow-md">
+                        <Image
+                            alt={song.title}
+                            className="aspect-square object-cover transition-all hover:scale-105"
+                            height={120}
+                            src={song.thumbnail}
+                            width={320}
+                        />
+                    </div>
+                    <div className="text-left">
+                        <UnescapedTitle
+                            className="text-xs tracking-tight"
+                            title={song.title}
+                        />
+                        <p className="text-xs text-muted-foreground">
+                            {song.channelTitle}
+                        </p>
+                    </div>
 
-            <div className="flex items-center gap-2">
-                {isCurrentPlaying && (
-                    <AudioLinesIcon className="h-5 w-5" />
-                )}
-                <AppPlayerMixSongMenu song={song} index={index} />
-            </div>
+                    <div className="flex items-center gap-2">
+                        {isCurrentPlaying ? (
+                            <AudioLinesIcon className="h-5 w-5" />
+                        ) : null}
+                        <AppPlayerMixSongMenu index={index} song={song} />
+                    </div>
+                </div>
+            </Button>
         </li>
+        // <li
+        //     className={cn(
+        //         "grid grid-cols-[40px_1fr_auto] items-center gap-3 rounded-sm border px-3 py-2 hover:cursor-pointer hover:bg-secondary",
+        //         index === currentIndex
+        //             ? "bg-secondary/50"
+        //             : "border-transparent",
+        //     )}
+        //     onClick={() => setCurrentIndex(index)}
+        // >
+        //     <div className="relative h-10 w-10 overflow-hidden rounded-md border-2 border-foreground drop-shadow-md">
+        //         <Image
+        //             alt={song.title}
+        //             className="aspect-square object-cover transition-all hover:scale-105"
+        //             height={120}
+        //             src={song.thumbnail}
+        //             width={320}
+        //         />
+        //     </div>
+        //     <div>
+        //         <UnescapedTitle
+        //             className="text-xs tracking-tight"
+        //             title={song.title}
+        //         />
+        //         <p className="text-xs text-muted-foreground">
+        //             {song.channelTitle}
+        //         </p>
+        //     </div>
+
+        //     <div className="flex items-center gap-2">
+        //         {isCurrentPlaying ? <AudioLinesIcon className="h-5 w-5" /> : null}
+        //         <AppPlayerMixSongMenu index={index} song={song} />
+        //     </div>
+        // </li>
     );
 };
 
