b037e7b52dc75738bdadee1d42acdab08ba02bf9
Joe Rosseljong <general@veepzy.net>
2024-07-19 21:25:46 +0200

    fix: import

--- src/features/app-player/types/player-ref.ts
+++ src/features/app-player/types/player-ref.ts
@@ -1,5 +1,6 @@
-import { ComponentType, RefObject } from "react";
-import ReactPlayer, { ReactPlayerProps } from "react-player";
+import { type ComponentType, type RefObject } from "react";
+import {type ReactPlayerProps} from "react-player";
+import type ReactPlayer from "react-player";
 
 export type PlayerRef = RefObject<
     ComponentType<ReactPlayerProps> & ReactPlayer
