445b4c270737d98284e86798aa99870a13584742
Joe Rosseljong <general@veepzy.net>
2024-07-19 21:25:53 +0200

    fix: auth sign in

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { authSignIn } from "@/db/actions";
+
 import { Button } from "./ui/button";
 
 const SignIn: React.FC = () => {
-    return <Button onClick={authSignIn}>Sign in</Button>;
+    return <Button onClick={() => authSignIn()}>Sign in</Button>;
 };
 
 export { SignIn };
