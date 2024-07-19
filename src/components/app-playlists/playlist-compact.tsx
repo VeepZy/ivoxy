da7c90e30c9355405e0ec8c759468248cbc7526e
Joe Rosseljong <general@veepzy.net>
2024-07-19 19:08:27 +0200

    feat: add more logic

--- src/components/app-playlists/playlist-compact.tsx
+++ src/components/app-playlists/playlist-compact.tsx
@@ -1,32 +1,60 @@
+"use client";
+
 import Image from "next/image";
 
 import { type SongData } from "@/db/types";
 import { UnescapedTitle } from "@/features/unescaped-title/components";
 
 import { PlaySongButton } from "../app-songs/song-play";
+import { usePlayerStore } from "@/stores/player";
+import { cn } from "@/lib/utils";
+import { AudioLinesIcon } from "../audio-icon";
+import { SongMenu } from "../app-songs/song-menu";
 
 const PlaylistCompact: React.FC<{ song: SongData }> = ({ song }) => {
+    const data = usePlayerStore((store) => store.data);
+    const isPlaying = usePlayerStore((store) => store.isPlaying);
+    const index = usePlayerStore((store) => store.currentIndex);
+    const setData = usePlayerStore((store) => store.setData);
+
+    const isCurrentPlaying = isPlaying && data?.[index]?.url === song.url;
+
     return (
-        <div className="grid grid-cols-[72px_1fr_auto] items-center gap-4 rounded-md p-3 hover:cursor-pointer hover:bg-secondary">
-            <div className="group relative overflow-hidden rounded-md border-2 border-foreground drop-shadow-md">
+        <li
+            onClick={() => setData([song])}
+            className={cn(
+                "grid grid-cols-[40px_1fr_auto] items-center gap-3 rounded-sm border px-3 py-2 hover:cursor-pointer hover:bg-secondary",
+                isCurrentPlaying
+                    ? "bg-secondary/50"
+                    : "border-transparent",
+            )}
+        >
+            <div className="relative h-10 w-10 overflow-hidden rounded-md border-2 border-foreground drop-shadow-md">
                 <Image
                     alt={song.title}
-                    className=" aspect-square h-auto w-auto object-cover transition-all hover:scale-105"
+                    className="aspect-square object-cover transition-all hover:scale-105"
                     height={120}
                     src={song.thumbnail}
-                    width={312}
+                    width={320}
                 />
             </div>
-            <div className="grid gap-1">
-                <UnescapedTitle title={song.title} />
+            <div>
+                <UnescapedTitle
+                    title={song.title}
+                    className="text-xs tracking-tight"
+                />
                 <p className="text-xs text-muted-foreground">
                     {song.channelTitle}
                 </p>
             </div>
+
             <div className="flex items-center gap-2">
-                <PlaySongButton song={song} />
+                {isCurrentPlaying && (
+                    <AudioLinesIcon className="h-5 w-5" />
+                )}
+                <SongMenu song={song} />
             </div>
-        </div>
+        </li>
     );
 };
 
