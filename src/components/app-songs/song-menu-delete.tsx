4d6c236ddee9f2ebb30404cf3444b9d0520c370e
Joe Rosseljong <general@veepzy.net>
2024-07-19 19:07:23 +0200

    feat: delete from playlist menu dropdown

---
+++ src/components/app-songs/song-menu-delete.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { ListMinusIcon, LoaderCircleIcon } from "lucide-react";
+
+import {
+    DropdownMenuItem,
+    DropdownMenuPortal,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
+import { updatePlaylist } from "@/db/actions";
+import { type Playlist, type SongData } from "@/db/types";
+import { useDataStore } from "@/hooks/use-data";
+import { cn } from "@/lib/utils";
+import { filterPlaylists } from "@/utils/filter";
+import { useTransition, MouseEvent } from "react";
+import { useToast } from "../ui/use-toast";
+
+const DropdownMenuDeleteFromPlaylist: React.FC<{ song: SongData }> = ({
+    song,
+}) => {
+    const [pending, startTransition] = useTransition();
+
+    const { playlists } = useDataStore();
+    const { toast } = useToast();
+
+    const filtered = filterPlaylists(playlists, song.url, true);
+
+    const onSubmit = (
+        event: MouseEvent<HTMLDivElement>,
+        playlist: Playlist,
+    ) => {
+        event.preventDefault();
+
+        startTransition(async () => {
+            const newData = playlist.data.filter(
+                (data) => data.url !== song.url,
+            );
+
+            const newArray = { ...playlist, data: newData };
+
+            await updatePlaylist(newArray);
+
+            toast({
+                title: "Success",
+                description: `${song.title} removed from ${playlist.name}`,
+            });
+        });
+    };
+
+    return (
+        <DropdownMenuSub>
+            <DropdownMenuSubTrigger
+                disabled={filtered.length === 0}
+                className={cn(
+                    filtered.length === 0 && "text-muted-foreground",
+                )}
+            >
+                <ListMinusIcon className="mr-2 h-5 w-5" />
+                Delete from Playlist
+            </DropdownMenuSubTrigger>
+
+            <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                    {filtered.map((playlist) => (
+                        <DropdownMenuItem
+                            key={playlist.id}
+                            disabled={pending}
+                            className="hover:cursor-pointer hover:bg-secondary"
+                            onClick={(event) => onSubmit(event, playlist)}
+                        >
+                            {playlist.name}
+                            {pending && (
+                                <LoaderCircleIcon className="ml-auto h-5 w-5 animate-spin text-primary" />
+                            )}
+                        </DropdownMenuItem>
+                    ))}
+                </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+        </DropdownMenuSub>
+    );
+};
+
+export { DropdownMenuDeleteFromPlaylist };
