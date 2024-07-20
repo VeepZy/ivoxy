70d16165691066937318dcbc363b839308cef598
Joe Rosseljong <general@veepzy.net>
2024-07-20 13:50:34 +0200

    use origin location

--- src/components/app-menu/auth-login.tsx
+++ src/components/app-menu/auth-login.tsx
@@ -3,9 +3,20 @@
 import { authSignIn } from "@/db/actions";
 
 import { MenubarItem } from "../ui/menubar";
+import { useMounted } from "@/hooks/mounted";
 
 const MenubarAuthLogin: React.FC = () => {
-    return <MenubarItem onClick={authSignIn}>Login</MenubarItem>;
+    const mounted = useMounted();
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <MenubarItem onClick={() => authSignIn(window.location.origin)}>
+            Login
+        </MenubarItem>
+    );
 };
 
 export { MenubarAuthLogin };
